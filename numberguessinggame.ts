51d7cee007733598b8847decf98f30d114e41566
Rehan <nawaz.abbasi.pk@gmail.com>
2024-04-24 16:48:29 +0500

    RehanNumberGuessingGame

---
+++ numberguessinggame.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+const Computerguessednumber = Math.floor(Math.random()*5+1)
+const userguessed = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type:"number",
+        message:"Guess The Number Between 1 and 5"
+
+}
+]);
+if(userguessed.UserGuessedNumber==Computerguessednumber){
+    console.log("Congrats The Number You Guess Was Correct")
+}else{
+    console.log("You Have Guessed Wrong Number")
+}
+
